d623b41caa771b64bed88d96ae9a6bf6879cd78b
Sonja Nyman <sonja.nyman@student.careeria.fi>
2021-07-21 14:07:52 +0300

    Vaihdoin login- nimen "kirjautuminen" nimeen

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,9 +27,36 @@ namespace Hyvinvointisovellus.Controllers
             return View();
         }
         
-        public ActionResult Login()
+        public ActionResult Kirjautuminen()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(Kirjautuminen LoginModel)
+        {
+            HyvinvointiDBEntities db = new HyvinvointiDBEntities();
+            //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ -kyselyllä
+            var LoggedUser = db.Kirjautuminen.SingleOrDefault(x => x.Kayttajatunnus == LoginModel.Kayttajatunnus && x.Salasana == LoginModel.Salasana);
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoggedStatus = "In";
+                Session["UserName"] = LoggedUser.Kayttajatunnus;
+                return RedirectToAction("Index", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa --> Home/Index
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                LoginModel.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                return View("Login", LoginModel);
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home"); //Uloskirjautumisen jälkeen pääsivulle
         }
 
     }
